fccf75be16e9c7121b9dea40572c2b3cfa760efb
zolanovich <avilajuan0301@gmail.com>
2022-07-05 19:11:38 -0400

    Clase userData

---
+++ CalculadoraIndiceAcademico/Models/userData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraIndiceAcademico.Models
+{
+    public class userData
+    {
+        public string ID { get; set;}
+        public string Rol { get; set;}
+        public string  Nombre { get; set;}
+        public string AreaAcademica { get; set;}
+    }
+}
--- CalculadoraIndiceAcademico/frmLogin.aspx.cs
+++ CalculadoraIndiceAcademico/frmLogin.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using CalculadoraIndiceAcademico.dsSCIATableAdapters;
+using CalculadoraIndiceAcademico.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -6,7 +7,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 namespace CalculadoraIndiceAcademico
 {
     public partial class frmLogin : System.Web.UI.Page
@@ -20,17 +20,24 @@ namespace CalculadoraIndiceAcademico
         {
             tblUsuariosTableAdapter usuario = new tblUsuariosTableAdapter();
             DataTable ds = usuario.GetDataByLogin(txtUsuario.Text, txtContraseña.Text);
+            
+            
+            userData data = new userData();
+            data.ID = ds.Rows[0][0].ToString();
+            data.Rol = ds.Rows[0][2].ToString();
+
             if (ds.Rows.Count == 1)
             {
                 switch (ds.Rows[0][2].ToString())
                 {
                     case "1":
+                        Response.Redirect("/frmPerfilEstudiante.aspx");
                         break;
                     case "2":
                         break;
                     case "3":
                         Response.Redirect("/frmMantenimientoAdministrador.aspx");
-                        break;
+                        break; 
                 }
             }
             else
